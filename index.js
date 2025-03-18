0c112b78b1bbf1f84659fca9a7e0947a96d50f4c
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2025-03-18 14:02:32 +0100

    Füge einfachen HTTP-Server hinzu, der zufällige Witze bereitstellt

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+// Eine Liste von Witzen
+const jokes = [
+  'Warum können Geister so schlecht lügen? Weil man durch sie hindurchsehen kann!',
+  'Warum können Geister keine Lügen erzählen? Weil sie durchsichtige Argumente haben!',
+  'Warum haben Elefanten keine Computer? Weil sie Angst vor der Maus haben!',
+  'Warum können Skelette keine Partys feiern? Weil sie niemanden haben, mit dem sie tanzen können!',
+];
+
+// Funktion, um einen zufälligen Witz auszuwählen
+function getRandomJoke() {
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  return jokes[randomIndex];
+}
+
+// Server erstellen
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/api/jokes') {
+    const joke = getRandomJoke();
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ joke }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+// Server auf Port 3000 starten
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
